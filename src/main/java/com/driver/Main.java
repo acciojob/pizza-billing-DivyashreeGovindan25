ac45288492b593601df776d555d88706d57d371d
HP <divyashreeharitha@gmail.com>
2024-01-15 13:52:02 +0530

    Running commit1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,16 +3,15 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
     DeluxePizza dp = new DeluxePizza(true);
-    System.out.println(dp.getPrice());
-    dp.addTakeaway();
-    dp.addExtraCheese();
+    //System.out.println(dp.getPrice());
     dp.addTakeaway();
     System.out.println(dp.getBill());
 
-    Pizza p = new Pizza(true);
+    System.out.println();
+
+    Pizza p = new Pizza(false);
     p.addExtraCheese();
     p.addExtraToppings();
-    p.addTakeaway();
     System.out.println(p.getBill());
   }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,20 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    //private boolean isDelux;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        //this.isDelux = isDelux;
+        bill = "";
+        if(isVeg){
+            price = 300;
+            bill += "Base Price Of The Pizza: 300";
+        }
+        else{
+            price = 400;
+            bill += "Base Price Of The Pizza: 400";
+        }
     }
 
     public int getPrice(){
@@ -16,19 +26,28 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+            price += 80;
+            bill += "\nExtra Cheese Added: 80";
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(isVeg){
+            price += 70;
+            bill += "\nExtra Toppings Added: 70";
+        }
+        else{
+            price += 120;
+            bill += "\nExtra Toppings Added: 120";
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        price += 20;
+        bill += "\nPaperbag Added: 20";
     }
 
     public String getBill(){
-        // your code goes here
+        bill += "\nTotal Price: " + price;
         return this.bill;
     }
 }
